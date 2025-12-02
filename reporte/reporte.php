62a0cf72f14b7743bd54848b9ad76bfb9cebc165
edwardnavas-di <edward.navas@integracion.gob.sv>
2025-12-01 21:57:23 -0600

    Summary

--- reporte/reporte.php
+++ reporte/reporte.php
@@ -4,7 +4,7 @@
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
-
+date_default_timezone_set('America/El_Salvador');
 session_start();
 
 require_once __DIR__ . '/../includes/config.php';
